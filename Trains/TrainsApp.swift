75d76c65cf61ec553be46c44600c6f60e2379fc4
Симонов Иван дмитриевич <fensiz@me.com>
2025-06-26 19:28:35 +0300

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Симонов Иван Дмитриевич on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
